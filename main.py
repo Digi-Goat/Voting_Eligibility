95efb321ad93e5445fc7cbd9085037a78e6d3361
Isabella Defiore <defioisa000@warren.k12.in.us>
2024-10-30 07:31:35 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+is_eligible = 18
+
+age = int(input("How old are you?"))
+
+if age >= is_eligible:
+    print("Congratulations! You are eligible to vote!")
+else:
+    print("You are not eligible to vote.")
